fb4790f4c90e2fdf36a9be95ac2e798fd8358a1e
Shireen Kachroo <kachrooshireen3@gmail.com>
2026-04-11 17:52:51 +0530

    Added main program flow

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "optimizer/Optimiser.h"
 #include "cost/CostEstimator.h"
 
-// Helper function to print the tree structure to the console
 void printTree(Node* root, int level = 0) {
     if (!root) return;
     for (int i = 0; i < level; i++) std::cout << "  ";
@@ -17,15 +16,12 @@ void printTree(Node* root, int level = 0) {
 }
 
 int main() {
-    std::cout << "==========================================" << std::endl;
-    std::cout << "      SQL QUERY OPTIMIZATION ENGINE       " << std::endl;
-    std::cout << "==========================================" << std::endl;
+    std::cout << " SQL QUERY OPTIMIZATION ENGINE " << std::endl;
 
-    // --- STEP 1: BUILD UNOPTIMIZED PLAN ---
+    // BUILD UNOPTIMIZED PLAN ---
     // Structure: LIMIT -> PROJECT -> SELECT -> JOIN -> (TABLE A, TABLE B)
     // Note: We put Table A (1000) on left and Table B (500) on right to test Reordering
-    
-    Node* tableA = new Node("TABLE", "A"); 
+    Node* tableA = new Node("TABLE", "A");
     Node* tableB = new Node("TABLE", "B");
 
     Node* join = new Node("JOIN");
@@ -44,28 +40,23 @@ int main() {
     std::cout << "\n[1] Initial Query Plan (Unoptimized):" << std::endl;
     printTree(root);
 
-    // --- STEP 2: CALCULATE INITIAL COST ---
+    // calc initial cost
     double initialCost = CostEstimator::estimateCost(root);
 
-    // --- STEP 3: RUN OPTIMIZATION PIPELINE ---
-    // This runs: Limit Pushdown -> Selection Pushdown -> Join Reordering -> Projection Pushdown
+    // run ptimization pipeline
     Node* optimizedRoot = Optimiser::optimise(root);
 
     std::cout << "\n[2] Optimized Query Plan:" << std::endl;
     printTree(optimizedRoot);
 
-    // --- STEP 4: CALCULATE FINAL COST & REPORT ---
+    // calc final cost and report
     double finalCost = CostEstimator::estimateCost(optimizedRoot);
     double improvement = ((initialCost - finalCost) / initialCost) * 100;
-
-    std::cout << "\n==========================================" << std::endl;
-    std::cout << "           OPTIMIZATION REPORT            " << std::endl;
-    std::cout << "==========================================" << std::endl;
+    std::cout << " OPTIMIZATION REPORT " << std::endl;
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "Initial Estimated Cost   : " << initialCost << std::endl;
     std::cout << "Optimized Estimated Cost : " << finalCost << std::endl;
     std::cout << "Efficiency Improvement   : " << improvement << "%" << std::endl;
-    std::cout << "==========================================" << std::endl;
 
     return 0;
 }
